2e7c4f0c03eeaa485a739b4671876347f075ad01
Jaron Kennel <jaronkk@gmail.com>
2015-01-08 11:54:12 -0500

    Added deploy scripts for deploying from Github.

---
+++ config/deploy.rb
@@ -0,0 +1,65 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'guide_on_the_side'
+set :repo_url, 'https://github.com/ndlib/Guide-on-the-Side.git'
+
+# Default branch is :master
+if ENV['BRANCH']
+  # Does not prompt for branch when it is set as an environment variable
+  # e.g. `BRANCH=hesburgh-master cap production deploy`
+  set :branch, ENV['BRANCH']
+elsif fetch(:stage).to_s == 'production'
+  # Default to the master branch when deploying to production
+  ask :branch, 'hesburgh-master'
+else
+  # Default to the current branch when deploying to other servers
+  ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+end
+
+# deploy_to is set by the environment
+set :deploy_to, proc { "#{fetch(:deploy_base)}/guide_on_the_side" }
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('app/webroot/uploads', 'app/tmp/logs', 'app/tmp/sessions', 'lib')
+
+set :php_bin_dir, proc { "#{fetch(:deploy_base)}/php/bin" }
+
+# Default value for default_env is {}
+set :default_env, proc { { path: "#{fetch(:php_bin_dir)}:$PATH" } }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
+
+after 'deploy:check', 'cakephp:check'
+before 'deploy:updated', 'cakephp:check'
